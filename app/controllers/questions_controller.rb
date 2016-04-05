3293441952bb6d41be6b84720c3266c977080859
Frederic Descamps <frederic.descamps@percona.com>
2016-04-05 14:32:14 +0200

    add level
    Signed-off-by: Frederic Descamps <frederic.descamps@percona.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -31,7 +31,7 @@ class QuestionsController < ApplicationController
 
   def results
     @options = @question.options
-    @qr = RQRCode::QRCode.new("http://192.168.1.2:3001/"+@question.secret).to_img.resize(300, 300).to_data_url
+    @qr = RQRCode::QRCode.new("http://192.168.1.2:3001/"+@question.secret, :size => 4, :level => :h).to_img.resize(300, 300).to_data_url
 
   end
 
