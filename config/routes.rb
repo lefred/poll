0f84a4d36def80933f0836942fab312d7adb1d64
Frederic Descamps <frederic.descamps@percona.com>
2016-04-05 07:43:33 +0200

    make the changes for PLMCE
    Signed-off-by: Frederic Descamps <frederic.descamps@percona.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :questions
-  get '/admin' => 'admin#dashboard'
+  get '/admin123' => 'admin#dashboard'
   put '/votes' => 'votes#update'
   get ':secret' => 'questions#show'
   get ':secret/results' => 'questions#results'
