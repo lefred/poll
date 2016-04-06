5e198a3d93f2bc94dea7e03f9d9c835ce3f0af3a
Frederic Descamps <frederic.descamps@percona.com>
2016-04-06 08:44:48 +0200

    add the question type with correct answers
    nothing is yet created to deal with those type of queries and correct/wrong answers
    but they are at least saved to database already
    
    Signed-off-by: Frederic Descamps <frederic.descamps@percona.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,11 +13,18 @@ class QuestionsController < ApplicationController
     @question.secret = SecureRandom.urlsafe_base64(nil, false)[0, 5] unless @question.secret?
     @question.save!
 
-    params[:options].each do |option|
+    params[:options].each_with_index do |option, index|
       if option[:title] != ""
         new_option = Option.new
         new_option.title = option[:title]
         new_option.question_id = @question.id
+        if params.include? :answer
+            if params[:answer][index.to_s] == "1" && @question.q_type == 2
+                new_option.correct = 1
+            elsif @question.q_type == 2
+                new_option.correct = 0
+            end
+        end
         new_option.save!
       end
     end
@@ -46,7 +53,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :secret)
+    params.require(:question).permit(:title, :secret, :q_type)
   end
 
   def check_secret_is_unique
